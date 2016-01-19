fdff97b4d783304579097e8976be42ef07d903d8
August Toman-Yih <atomanyih@pivotal.io>
2016-01-18 16:33:20 -0800

    copy helpers

---
+++ src/spy_on_render.js
@@ -0,0 +1,44 @@
+module.exports = {
+  spyOnRender(componentClass) {
+    return spyOn(componentClass.prototype, 'render');
+  },
+  customMatchers: {
+    toHaveBeenRenderedWithProps(util, customEqualityTesters) {
+      return {
+        compare(actual, expected) {
+          let result = {};
+
+          const mostRecentCall = actual.prototype.render.calls.mostRecent();
+
+          const displayClass = actual.displayName;
+          const displayExpected = jasmine.pp(expected);
+
+
+          if (mostRecentCall) {
+            const actualProps = mostRecentCall.object.props;
+            result.pass = util.equals(
+              actualProps,
+              expected,
+              customEqualityTesters
+            );
+
+
+            if (result.pass) {
+              result.message = `Expected ${displayClass} not to have been rendered with props ${displayExpected}`;
+            } else {
+              const displayActual = jasmine.pp(actualProps);
+
+              result.message = `Expected ${displayClass} to have been rendered with props ${displayExpected}, but got ${displayActual}`;
+            }
+          } else {
+            result.pass = false;
+
+            result.message = `Expected ${displayClass} to have been rendered with props ${displayExpected}, but it was never rendered`;
+          }
+
+          return result;
+        }
+      };
+    }
+  }
+};
